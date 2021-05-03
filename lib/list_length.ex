2a535ba171da8f7fd164a7a78f54d20ab146d94c
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-05-03 20:35:25 -0300

    :sparkles: adds the challenge

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,18 +1,40 @@
 defmodule ListLength do
   @moduledoc """
-  Documentation for `ListLength`.
+  Ignite first challenge.
   """
 
+  @spec call(list) :: non_neg_integer
   @doc """
-  Hello world.
+  Calculates how many elements are in a list.
 
   ## Examples
 
-      iex> ListLength.hello()
-      :world
+      iex> ListLength.call([1, 2, 3, 4, 5, 6])
+      6
+      iex> ListLength.call([1, 2, 3, 4, 5, 6, 7, 8])
+      8
+      iex> ListLength.call([1, 1])
+      2
+      iex> ListLength.call([])
+      0
 
   """
-  def hello do
-    :world
+
+  # Nesse desafio, você deverá criar uma função que, dada uma lista de números (exemplo `[1, 3, 6, 43, 6]`), calcule, **recursivamente**, quantos elementos há na lista e retorne esse valor.
+
+  # Para exemplo, temos a seguinte entrada e saída esperada pela função que recebe a lista:
+
+  # ```bash
+  # iex> ListLength.call([1, 2, 3, 5, 7])
+  # ...> 5
+  # ```
+  def call(list), do: length(list, 0)
+
+  defp length([], acc), do: acc
+
+  defp length([_head | tail], acc) do
+    acc = acc + 1
+
+    length(tail, acc)
   end
 end
