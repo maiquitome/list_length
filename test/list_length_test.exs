df83e5722a4569685f77252a174e8726b31758b1
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-05-03 20:35:41 -0300

    :white_check_mark: adds the test

--- test/list_length_test.exs
+++ test/list_length_test.exs
@@ -2,7 +2,15 @@ defmodule ListLengthTest do
   use ExUnit.Case
   doctest ListLength
 
-  test "greets the world" do
-    assert ListLength.hello() == :world
+  describe "call/1" do
+    test "Returns how many elements are in a list." do
+      list = [1, 2, 3, 4, 5]
+
+      response = ListLength.call(list)
+
+      expected_response = 5
+
+      assert response == expected_response
+    end
   end
 end
